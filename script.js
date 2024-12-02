1eb46848b9cad682720ff81aacc08b182c6be4a8
Khushi-Gupta-513 <khushigupta050103@gmail.com>
2024-12-02 17:38:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const ctx = document.getElementById('barChart').getContext('2d');
 new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow"],
+        labels: ["Red", "Blue", "Yellow", "Black"],
         datasets: [
             {
                 label: "Votes",
-                data: [12, 9, 3],
+                data: [12, 9, 3, 10],
                 backgroundColor: ["red", "blue", "yellow"],
                 borderColor: ["darkred", "darkblue", "gold"],
                 borderWidth: 2
