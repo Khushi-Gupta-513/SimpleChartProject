99477f02e884c5469dc12a416910f94090414d3c
Khushi-Gupta-513 <khushigupta050103@gmail.com>
2024-12-02 17:22:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+script.js
+const ctx = document.getElementById(barChart)
 
+new Chart(
+    ctx, {
+        type: 'bar'
+        data: {
+            labels: ["Red","Blue","Yellow"],
+            datasets: [
+                {
+                    label: "Votes",]
+                    data: [12,9,3], 
+                    backgroundColor: ["Red","Blue","Yellow"]
+    
+                    ]
+                }
+            ]
+        }
+    }
+
+)
